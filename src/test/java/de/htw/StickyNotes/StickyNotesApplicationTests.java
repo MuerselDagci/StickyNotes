04c9384d57bc2791999a9782c3f738cb224eaf58
MuerselDagci <Muersel.Dagci@Student.HTW-Berlin.de>
2022-04-27 01:40:56 +0200

    init

---
+++ src/test/java/de/htw/StickyNotes/StickyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.StickyNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
